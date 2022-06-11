7ce1374fdc991d8d7d8318d2a8fec947323ea6f4
Taras-Roboteckyi <rtvr@ukr.net>
2022-06-11 21:35:48 +0300

    add pagination

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -1,9 +1,8 @@
 const { User } = require("../../models/user");
-const createError = require("http-errors");
 
 const logOut = async (req, res, next) => {
   const { _id } = req.user;
-  const deleteToken = await User.findByIdAndUpdate(_id, { token: null });
+  await User.findByIdAndUpdate(_id, { token: null });
   res.status(204).json();
 };
 
